2105465a40eaf919cbab60b42fe4c880adeeff40
amirulSanz17 <amirulsanz17@gmail.com>
2026-06-01 05:25:54 +0700

    fix .env

--- config/database.php
+++ config/database.php
@@ -1,8 +1,10 @@
 <?php
-$host = 'localhost';
-$dbname = 'rongsokhub';
-$username = 'root';
-$password = '';
+$env = parse_ini_file(__DIR__ . '/../.env');
+
+$host = $env['DB_HOST'];
+$dbname = $env['DB_NAME'];
+$username = $env['DB_USER'];
+$password = $env['DB_PASS'];
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
